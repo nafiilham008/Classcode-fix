02b9c011acdb3ac65036811bd964d850561fdd6e
nafiilham_h <nafiilham.h@gmail.com>
2022-11-14 18:12:19 +0700

    fix role mentor

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Config;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -20,12 +22,17 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'alamat',
         'password',
-        'role'
     ];
+
+    // public function roles()
+    // {
+    //     return $this->belongsToMany(Role::class);
+    // }
+
 
     /**
      * The attributes that should be hidden for serialization.
